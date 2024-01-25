839cf7654a1b031b8ea209d568993ab70212ac2b
Doug <wanderlustking11@gmail.com>
2024-01-25 06:17:58 -0500

    Practice/refactor twoSum & linkedListAddTwo

--- LC-0001-twoSum.js
+++ LC-0001-twoSum.js
@@ -17,72 +17,108 @@
 // target is also an input
 
 // V1.0 with Redward
-var twoSum = function(nums, target) {
-  for (var i = 0; i < nums.length; i++) {
-    for (var k = i; k < nums.length; k++) {
-      if (i !== k) {
-        sum = nums[i] + nums[k];
-        if (sum === target) {
-          return [i, k];
-        }
-      }
-    }
-  }
-}
+// var twoSum = function(nums, target) {
+//   for (var i = 0; i < nums.length; i++) {
+//     for (var k = i; k < nums.length; k++) {
+//       if (i !== k) {
+//         sum = nums[i] + nums[k];
+//         if (sum === target) {
+//           return [i, k];
+//         }
+//       }
+//     }
+//   }
+// }
 
-// V2.0 - Viisheshjain05
-var twoSum = function(nums, target) {
-  let total = 0;
-  for (let i = 0; i < nums.length; i++) {
-    // if (nums[i] <= target) {
-    for (let j = i + 1; j <= nums.length - 1; j++) {
-      total = nums[i] + nums[j];
-      if (total == target) {
-        return [i, j];
-      }
-    }
-    // }
-  }
-};
+// // V2.0 - Viisheshjain05
+// var twoSum = function(nums, target) {
+//   let total = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     // if (nums[i] <= target) {
+//     for (let j = i + 1; j <= nums.length - 1; j++) {
+//       total = nums[i] + nums[j];
+//       if (total == target) {
+//         return [i, j];
+//       }
+//     }
+//     // }
+//   }
+// };
 
-// V3.0
+// // V3.0
+// function twoSum(nums, target) {
+//   var sum = 0;
+//   for (var i = 0; i < nums.length; i++) {
+//     for (var k = i + 1; k < nums.length; k++) {
+//       sum = nums[i] + nums[k];
+//       if (sum === target) {
+//         return [i, k];
+//       }
+//     }
+//   }
+// }
+
+// // v4.0 fastest
+// var twoSum = function(nums, target) {
+//   let cache = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let remaining = target - nums[i];
+
+//     if (cache[nums[i]] !== undefined) {
+//       return [cache[nums[i]], i];
+//     }
+
+//     cache[remaining] = i;
+//   }
+// };
+
+// ** Note ** Because cache is an object, I tried
+// using dot notation, but the property names (keys)
+// are determined dynamically during runtime (the
+// values of 'remaining' and 'nums[i]'). Since these
+// keys are numbers and are stored in variables, we 
+// have to use bracket notation.
+
+// For example, we cannot replace `cache[remaining] = i;`
+// with `cache.remaining = i;` because the latter
+// literally set a property named "remaining" on the
+// object, not a property whose name is the value of
+// the `remaining` variable.
+
+// // V5.0 HashMap (The exact same as V4.0, but with
+// // the use of 'in' keyword)
+
+// var twoSum = function(nums, target) {
+//   const hashMap = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const diff = target - nums[i];
+
+//     if (diff in hashMap) {
+//       return [hashMap[diff], i]
+//     }
+//     hashMap[nums[i]] = i
+//   }
+// }
+
+
+/////////////////////////////////////////////////
+
+/////////// Practice ///////////////
+
+const nums = [3, 5, 22, 10];
+const target = 15;
+
 function twoSum(nums, target) {
-  var sum = 0;
-  for (var i = 0; i < nums.length; i++) {
-    for (var k = i + 1; k < nums.length; k++) {
-      sum = nums[i] + nums[k];
-      if (sum === target) {
-        return [i, k];
-      }
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const secondNum = target - nums[i];
+    if (map.nums[i] !== undefined) {
+      console.log([map.nums[i], i]);
     }
+    map.secondNum = i;
   }
 }
 
-// v4.0 fastest
-var twoSum = function(nums, target) {
-  let cache = {};
-
-  for (let i = 0; i < nums.length; i++) {
-    let remaining = target - nums[i];
-
-    if (cache[nums[i]] !== undefined) {
-      return [cache[nums[i]], i];
-    }
-
-    cache[remaining] = i;
-  }
-};
-
-// V5.0 HashMap
-var twoSum = function(nums, target) {
-  const hashMap = {};
-
-  for (let i = 0; i < nums.length; i++) {
-    const diff = target - nums[i];
-
-    if (diff in hashMap) {
-      return [hashMap[diff], i]
-    }
-    hashMap[nums[i]] = i
-  }
-}
+twoSum(nums, target);
--- LC-0002-linkedListAddTwo.js
+++ LC-0002-linkedListAddTwo.js
@@ -1,44 +1,84 @@
 // Medium Add Two numberGroups
-// You are give two non-empty linked lists representing two non-negative integers. The digits are stored in reverse ondragover, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+// You are give two non-empty linked lists 
+// representing two non-negative integers. 
+// The digits are stored in reverse ondragover, 
+// and each of their nodes contains a single digit. 
+// Add the two numbers and return the sum as a linked
+// list.
 
-// You may assume the two numbers do not contain any leading zero, except the number of 0 itself.Add
+// You may assume the two numbers do not contain any 
+// leading zero, except the number of 0 itself.
 
-// The definition for each list node is that it has 2 properties: val, and next
+// The definition for each list node is that it has 
+// 2 properties: val, and next
 
 // Example:
 // Input: l1 = [2,4,3], l2 = [5,6,4]
 // Output: [7,0,8]
 // Explanation: 342 + 465 = 807.
 
-const addTwoNumbers = function(l1, l2) {
-  let current1 = l1;
-  let current2 = l2;
-  let head;
-  let previousNewNode;
-  let sum = 0;
-  while(current1 || current2 || sum === 1) {
+// const addTwoNumbers = function(l1, l2) {
+//   let current1 = l1;
+//   let current2 = l2;
+//   let head;
+//   let previousNewNode;
+//   let sum = 0;
+//   while(current1 || current2 || sum === 1) {
     
-    if (current1) {
-      sum += current1.val;
-      current1 = current1.next; // Basic iteration of list 1
-    }
-    if (current2) {
-      sum += current2.val;
-      current2 = current2.next; // iterate list 2
-    }
+//     if (current1) {
+//       sum += current1.val;
+//       current1 = current1.next; // Basic iteration of list 1
+//     }
+//     if (current2) {
+//       sum += current2.val;
+//       current2 = current2.next; // iterate list 2
+//     }
 
-    const newNode = new ListNode(sum % 10);
+//     const newNode = new ListNode(sum % 10);
 
-    sum = Math.floor(sum/10); // helps carry over +1 to next through the loop
+//     sum = Math.floor(sum/10); // helps carry over +1 to next through the loop
 
-    if(!head) {
-      head = newNode;
-      previousNewNode = newNode;
-    } else {
-        previousNewNode.next = newNode;
-        previousNewNode = previousNewNode.next;
-    }
+//     if(!head) {
+//       head = newNode;
+//       previousNewNode = newNode;
+//     } else {
+//         previousNewNode.next = newNode;
+//         previousNewNode = previousNewNode.next;
+//     }
+//   }
+
+//   return head;
+// };
+
+///////////////////////////////////////////
+
+////////// Practice ////////////
+
+const l1 = [2,4,3];
+const l2 = [5,6,4];
+
+function addTwoNumbers(l1, l2) {
+  let dummyHead = new ListNode(0);
+  let current = dummyHead;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null) {
+    let x = (l1 !== null) ? l1.val : 0;
+    let y = (l2 !== null) ? l2.val : 0;
+    let sum = carry + x + y;
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+
+    if (l1 != null) l1 = l1.next;
+    if (l2 != null) l2 = l2.next;
   }
 
-  return head;
-};
+  if (carry > 0) {
+    current.next = new ListNode(carry);
+  }
+
+  return dummyHead.next;
+}
+
+addTwoNumbers(l1, l2);
