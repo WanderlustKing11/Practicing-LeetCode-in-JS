3b540a309e76843c7cd34a8e2114df12be5f5388
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-02-13 18:18:41 -0500

    Update palindrome

--- LC-0009-PalindromeNumber.js
+++ LC-0009-PalindromeNumber.js
@@ -4,3 +4,23 @@
 // An integer is a palindrome when it reads the same backward as forward.
 //
 // For example, 121 is a palindrome while 123 is not.
+
+
+const x = 12321;
+
+const palindrome = (x) => {
+  const arr = Array.from(String(x));
+  let [left, right] = [0, arr.length - 1];
+  while (left < right) {
+    if (arr[left] !== arr[right]) {
+      console.log(false, "Not a Palindrome!");
+      return false;
+    }
+    left++;
+    right--;   
+  }
+  console.log(true, "Is Palindrome!");
+  return true;
+}
+
+palindrome(x);
